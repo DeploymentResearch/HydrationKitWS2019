49b7eb813337a3dbb7b61d7125bc567ceb608513
Andrew Johnson <15828090+AndrewJNet@users.noreply.github.com>
2022-01-25 21:27:48 -0600

    Update New-HydrationKitSetup.ps1

--- New-HydrationKitSetup.ps1
+++ New-HydrationKitSetup.ps1
@@ -17,7 +17,7 @@
     Disclaimer: This script is provided "AS IS" with no warranties, confers no rights and 
     is not supported by the author or DeploymentArtist..
 
-    .PARAMETER Path 
+    .PARAMETER Path
     Specifies the path for Hydration Kit installation directory.
     
     .PARAMETER ShareName
