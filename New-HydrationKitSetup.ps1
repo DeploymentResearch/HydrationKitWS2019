e1921f3c7a0a8e1de0eebf4a9b27069ae9ba3bef
Johan Arwidmark <arwidmark@gmail.com>
2022-02-25 16:13:55 -0600

    Update New-HydrationKitSetup.ps1

--- New-HydrationKitSetup.ps1
+++ New-HydrationKitSetup.ps1
@@ -140,6 +140,7 @@ New-Item -Path "$Path\DS\Operating Systems\WS2019\sources\sxs" -ItemType Directo
 # Create target folder structure for application sources
 New-Item -Path "$Path\DS\Applications\Install - SQL Server Management Studio" -ItemType Directory -Force
 New-Item -Path "$Path\DS\Applications\Install - Windows ADK 11\Source" -ItemType Directory -Force
+New-Item -Path "$Path\DS\Applications\Install - Windows ADK 11 WinPE Addon\Source" -ItemType Directory -Force
 New-Item -Path "$Path\DS\Applications\Install - SQL Server 2017\Source" -ItemType Directory -Force
 New-Item -Path "$Path\DS\Applications\Install - ConfigMgr\Source" -ItemType Directory -Force
 New-Item -Path "$Path\DS\Applications\Install - ConfigMgr\PreReqs" -ItemType Directory -Force
